f716d4784d2cf81d76d62319a388d9310168646f
bhagyashree-watpade <bhagyashreewatpade@gmail.com>
2025-02-06 22:35:01 +0530

    added ass2 component to index.js

--- appp/src/index.js
+++ appp/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Onclick from './Ass1';
+import Siblings from './Ass2';
 import Display from './Ass3';
 import UseRef from './Ass4';
 import UserInformation from './Ass5';
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Onclick />
+    <Siblings />
     <Display />
     <UseRef />
     <UserInformation />
